947117b56b9fc7d116bd61f7b3e1d6d49f1dd983
Thien Nguyen <thien.nguyen@seldatinc.com>
2016-05-13 17:49:25 +0700

    Original

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group([
+    'prefix' => 'user'
+], function() {
+    Route::get('register', [
+        'uses' => 'UserController@register'
+    ]);
+});
